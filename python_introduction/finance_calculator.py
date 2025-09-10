c00bb0538a1960ece22a204c0449966802ec690e
Favour Japheth <favourjaphet27@gmail.com>
2025-09-10 03:30:02 -0700

    Fix calculation and output formatting in finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,5 +7,5 @@ monthly_savings = income - expenses
 annual_savings = monthly_savings * 12
 projected_savings = annual_savings + (annual_savings * 0.05) 
 # Display the results
-print(f"your monthly savings are $ {monthly_savings}.")
-print(f"Projected savings after one year , with interest, is: ${projected_savings}.")
+print(f"your monthly savings are $ {monthly_savings}.0f")
+print(f"Projected savings after one year , with interest, is: ${projected_savings:.0f}.")
