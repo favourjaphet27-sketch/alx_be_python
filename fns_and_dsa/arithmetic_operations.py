d33e1bf6722879b749b484636eb257e76a7b8281
Favour Japheth <favourjaphet27@gmail.com>
2025-09-27 13:54:55 -0700

    Fix divide by zero message with exact wording

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
             return num1 * num2
         case "divide":
             if num2 == 0:
-                return "Cannot divide by zero"
+                return "Division by zero error"
             return num1 / num2
         case _:
             return "Error: Invalid operation. Use add, subtract, multiply, or divide."
